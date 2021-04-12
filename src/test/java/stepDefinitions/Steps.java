f810ff829eaaa20d997e66884d63352233f29f3d
shailntr2020 <shailntr@gmail.com>
2021-04-12 17:57:28 +0530

    message

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -77,31 +77,7 @@ public class Steps {
 		public void afterScenario(Scenario scenario) throws IOException {
 			
 			Commonfunction.getScreenShots(driver, scenario);
-		/*	if (scenario.isFailed()) {
-				String screenshotName = scenario.getName().replaceAll(" ", "_");
-				try {
-					//This takes a screenshot from the driver at save it to the specified location
-					TakesScreenshot ts= (TakesScreenshot)driver;
-					File sourcePath = ts.getScreenshotAs(OutputType.FILE);
-					//Building up the destination path for the screenshot to save
-					//Also make sure to create a folder 'screenshots' with in the cucumber-report folder
-					Date d = new Date();
-					Timestamp t = new Timestamp(d.getTime());
-					String timeStamp = t.toString();
-					timeStamp = timeStamp.replace(' ', '_');
-					timeStamp = timeStamp.replace(':', '_');
-					
-					String dest = System.getProperty("user.dir")+"/target/ScreenShots/failscreen"+ timeStamp +".png";
-					System.out.println(dest);
-					File destinationPath = new File(dest); 					
-					//Copy taken screenshot from source location to destination location
-					Files.copy(sourcePath, destinationPath);   
-
-					//This attach the specified screenshot to the test
-					Reporter.addScreenCaptureFromPath(destinationPath.toString());
-				} catch (IOException e) {
-				} 
-			}*/
+	
 		}
 	
 	
